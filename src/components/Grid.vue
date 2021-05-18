07f3033476116bb3ef5e67e61866594c10df0772
courtney l <clandau@learnics.com>
2021-05-18 14:14:48 -0400

    initial commit

---
+++ src/components/Grid.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="grid" :style="gridStyle">
+      <div v-for="pixelRow in pixels" :key="pixelRow">
+        <div
+          v-for="pixel in pixelRow"
+          :key="pixel"
+          :class="'box box' + pixel"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Grid",
+  data() {
+    return {
+      pixels: [
+        [1, 1, 1],
+        [1, 1, 0],
+        [1, 0, 1],
+      ],
+      pixelSize: "100px"
+    };
+  },
+
+  computed: {
+    gridStyle() {
+      const columns = this.pixels[0].length;
+      return {
+        display: "grid",
+        "justify-content": "center",
+        "align-content": "center",
+        "grid-template-columns": `repeat(${columns}, ${this.pixelSize}`
+      }
+    }
+  }
+});
+</script>
+
+<style scoped>
+.box {
+  min-width: 25px;
+  height: 100px;
+  border-style: solid;
+  border-width: 2px;
+  border-color: navy;
+}
+.box0 {
+  background-color: aquamarine;
+}
+.box1 {
+  background-color: orangered;
+}
+</style>
