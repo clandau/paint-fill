5b386d8a668eaf3aff9e9a5f82c648bc199c3f6f
courtney l <clandau@learnics.com>
2021-05-18 15:09:48 -0400

    added box ids, added click events and function to change colors, not yet working

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <div class="grid" :style="gridStyle">
-      <div v-for="pixelRow in pixels" :key="pixelRow">
+      <div v-for="(pixelCol, i) in pixels" :key="pixelCol">
         <div
-          v-for="pixel in pixelRow"
+          v-for="(pixel, j) in pixelCol"
           :key="pixel"
           :class="'box box' + pixel"
+          :id="`${j}-${i}`"
+          v-on:click="handleColorChange"
         ></div>
       </div>
     </div>
@@ -19,34 +21,76 @@ export default defineComponent({
   data() {
     return {
       pixels: [
-        [1, 1, 1],
-        [1, 1, 0],
-        [1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 0, 0],
+        [1, 0, 0, 1, 1, 0, 1, 1],
+        [1, 2, 2, 2, 2, 0, 1, 0],
+        [1, 1, 1, 2, 2, 0, 1, 0],
+        [1, 1, 1, 2, 2, 2, 2, 0],
+        [1, 1, 1, 1, 1, 2, 1, 1],
+        [1, 1, 1, 1, 1, 2, 2, 1],
       ],
-      pixelSize: "100px"
+      pixelSize: "30px",
     };
   },
 
   computed: {
     gridStyle() {
-      const columns = this.pixels[0].length;
+      const columns: number = this.pixels[0].length;
       return {
         display: "grid",
         "justify-content": "center",
         "align-content": "center",
-        "grid-template-columns": `repeat(${columns}, ${this.pixelSize}`
+        "grid-template-columns": `repeat(${columns}, ${this.pixelSize}`,
+      };
+    },
+  },
+
+  methods: {
+    handleColorChange(e: MouseEvent) {
+      const target = e.target as HTMLTextAreaElement;
+      console.log(target.id);
+      const [x, y] = target.id.split("-").map(i => parseInt(i));
+      this.paintFill(x, y, 2)
+    },
+
+    paintFill(x: number, y: number, newColor: number) {
+      let currentVal = this.pixels[x][y];
+      // set currentVal to newColor
+      this.pixels[x][y] = newColor;
+
+      // check top, bottom, left and right
+      // if they match currentVal, call function with that val's coordinates
+      // top
+      if (x - 1 >= 0 && this.pixels[x - 1][y] === currentVal) {
+        this.paintFill(x - 1, y, newColor);
       }
-    }
-  }
+      // bottom
+      if (x + 1 < this.pixels.length && this.pixels[x + 1][y] === currentVal) {
+        this.paintFill(x + 1, y, newColor);
+      }
+      // left
+      if (y - 1 >= 0 && this.pixels[x][y - 1] === currentVal) {
+        this.paintFill(x, y - 1, newColor);
+      }
+      // right
+      if (
+        y + 1 < this.pixels[x].length &&
+        this.pixels[x][y + 1] === currentVal
+      ) {
+        this.paintFill(x, y + 1, newColor);
+      }
+      return;
+    },
+  },
 });
 </script>
 
 <style scoped>
 .box {
-  min-width: 25px;
-  height: 100px;
+  height: 30px;
   border-style: solid;
-  border-width: 2px;
+  border-width: 1px;
   border-color: navy;
 }
 .box0 {
@@ -54,5 +98,11 @@ export default defineComponent({
 }
 .box1 {
   background-color: orangered;
+}
+.box2 {
+  background-color: whitesmoke;
+}
+.box3 {
+  background-color: plum;
 }
 </style>
